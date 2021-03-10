c014ea0b431a584acb3fcb711bbdabfa6e70f967
Mark Johnson <markandersjohnson@gmail.com>
2021-03-09 19:03:20 -0600

    cleaned up tailwinds

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Base Theme
 
 Very basic starter theme for client sites.
+
+# Asher Agenda
+are you cool hand-writing your name?
+maybe also the headers for each section?
--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -1,44 +1,13 @@
 const themeDir = __dirname + '/../../';
 
-const purgecss = require('@fullhuman/postcss-purgecss')({
-
-    // Specify the paths to all of the template files in your project
-    content: [
-        themeDir + 'layouts/**/*.html',
-        themeDir + 'content/**/*.html',
-        'layouts/**/*.html',
-        'content/**/*.html',
-        'assets/**/*.svg'
-    ],
-
-    // This is the function used to extract class names from your templates
-    defaultExtractor: content => {
-        // Capture as liberally as possible, including things like `h-(screen-1.5)`
-        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-
-        // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
-
-        return broadMatches.concat(innerMatches)
-    }
-})
-
 module.exports = {    
-    plugins: [        
+    plugins: [   
         require('postcss-import')({
             path: [themeDir]
-        }), 
+            }), 
         require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
         require('autoprefixer')({
             path: [themeDir]
         }),
-        require('postcss-for')({
-            path: [themeDir]
-        }),
-        require('precss')({
-            path: [themeDir]
-        }),
-        
-        ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
     ]
 }
